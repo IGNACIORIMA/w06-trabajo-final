65d57dd2e98dd847264ac3d8db4d88f5daa8107b
Ignacio Ribadeneira <ignaciormaggi@gmail.com>
2024-08-22 19:38:41 -0500

    test de login correcto y fallido, test de update con user2

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,6 +4,8 @@ const { use } = require("../routes")
 
 const BASE_URL = '/api/v1/users'
 let TOKEN 
+let TOKEN2
+let user2Id
 
 beforeAll(async()=> {
     const user = {
@@ -15,11 +17,10 @@ beforeAll(async()=> {
         .send(user)
     
     TOKEN = res.body.token
-    console.log(TOKEN)
+    /* console.log(TOKEN) */
 })
 
-
-const user = {
+const user2 = {
     firstName: "Iuvil",
     lastName: "Pena",
     email: "iuvil@gmail.com",
@@ -28,21 +29,75 @@ const user = {
 }
 
 //1. POST
-test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName', async ()=> {
-
-    const columns = [ 'firstName', 'lastName', 'email', 'password', 'phone']
+test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName', async () => {
 
     const res = await request(app)
         .post(BASE_URL)
-        .send(user)
+        .send(user2)
     
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
-    // columns.forEach((column) => {
-    //  })
     expect(res.body.firstName).toBeDefined()
-    expect(res.body.firstName).toBe(user.firstName)
+    expect(res.body.firstName).toBe(user2.firstName)
 })
+
+//.1 LOGIN OK 
+test('POST -> BASE_URL/login, should return statusCode 200, and res.body.user.firstName === user2.firstName', async() => {
+    const hits = {
+        email: "iuvil@gmail.com",
+        password: "iuvil1234"
+    }
+    const res = await request(app)
+        .post(`${BASE_URL}/login`)
+        .send(hits)
+    TOKEN2 = res.body.token
+    user2Id = res.body.user.id
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.user.firstName).toBe(user2.firstName)
+});
+
+// .2 LOGIN validando error
+test('POST -> BASE_URL/login, should return statusCode 401', async() => {
+    const hits = {
+        email: "iuvil@gmail.com",
+        password: "iuvil1224"
+    }
+    const res = await request(app)
+        .post(`${BASE_URL}/login`)
+        .send(hits)
+    expect(res.statusCode).toBe(401)
+});
+
+
 //2. GET
+test('GET -> BASE_URL, should return statuscode 200, and res.body.length === 2', async () => {
+    const res = await request(app)
+        .get(BASE_URL)
+        .set('Authorization', `Bearer ${TOKEN2}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(2)
+})
+
 //3. UPDATE
+test('UPDATE -> BASE_URL/:id, should return ', async () => {
+    const user2Upd = {
+        firstName: 'Luvil',
+        email: 'luvil@gmail.com',
+        password: 'luvil1234',
+        phone: '+593666554454'
+    };
+
+    const res = await request(app)
+        .put(`${BASE_URL}/${user2Id}`)
+        .send(user2Upd)
+        .set('authorization', `Bearer ${TOKEN2}`)
+    console.log(res)
+    expect(res.statusCode).toBe(200)
+    expect(res.body.firstName).toBe(user2Upd.firstName)
+})
+
 //4. DELETE
+test('UPDATE -> BASE_URL/:id, shoul return')
